89fcb72586775db62666563a1ca54ae26058ec39
Cuit <22183167+Mistakn@users.noreply.github.com>
2021-09-13 18:54:12 -0500

    09/13/2021

---
+++ Vue/primer-proyecto-vue-cli/src/components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Prototyping</h1>
+    <h1>{{ nombre }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nombre: "Cuit Rodriguez",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
